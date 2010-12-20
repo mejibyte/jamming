0b51cf914c388c18f782a067efbce8a4054fb8fb
Andrés Mejía <andmej@gmail.com>
2010-12-20 01:02:19 +0100

    Don't add a fifth fret when it's not strictly necessary. Minor tweaks.

--- lib/jamming/png_formatter.rb
+++ lib/jamming/png_formatter.rb
@@ -15,9 +15,9 @@ module Jamming
       
       @max_fret = @frets.compact.max
       @min_fret = @frets.compact.delete_if { |f| f == 0 }.min
-      @min_fret = 1 if @max_fret < 4
+      @min_fret = 1 if @max_fret <= 4
 
-      @max_dist = [@max_fret - @min_fret + 1, 3].max
+      @number_of_frets = [@max_fret - @min_fret + 1, 4].max
       
       get_png_data
     end
@@ -32,25 +32,25 @@ module Jamming
         canvas.background_fill = 'white'
         x_div = @strings.size - 1
         
-        width_of_chord = 280
+        width_of_chord = 260
         margin_side_of_chord = (width - width_of_chord) / 2
 
         height_of_chord = 200
         margin_top_of_chord = ((height - height_of_chord) * 2 / 3.0).floor
         margin_bottom_of_chord = ((height - height_of_chord) / 3.0).ceil
         
-        height_of_fret = height_of_chord / (@max_dist + 1)
+        height_of_fret = height_of_chord / @number_of_frets
         radius_of_finger = (height_of_fret * 0.6) / 2
         
         width_of_fret = width_of_chord / x_div
 
         # Draw all horizontal lines
-        (@max_dist+2).times do |n|
+        (@number_of_frets+1).times do |n|
           canvas.line(margin_side_of_chord, n*height_of_fret+margin_top_of_chord, width - margin_side_of_chord, n*height_of_fret+margin_top_of_chord)
         end
         
-        (@max_dist+1).times do |i|
-          canvas.text(margin_side_of_chord - 16, i*height_of_fret+margin_top_of_chord + height_of_fret / 2 + 10) do |txt|
+        (@number_of_frets).times do |i|
+          canvas.text(margin_side_of_chord - radius_of_finger - 4, i*height_of_fret+margin_top_of_chord + height_of_fret / 2 + 10) do |txt|
             txt.tspan(@min_fret + i).styles(
               :text_anchor => 'end',
               :font_size => 24,
